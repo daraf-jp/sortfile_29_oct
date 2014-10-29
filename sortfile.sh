677baa2f97250e474201ec48bfe44125131dd636
kazukitash <takahashi-k@daraf.co.jp>
2014-10-29 09:19:23 +0900

    議事録内のファイルを表示するようにした。

--- sortfile.sh
+++ sortfile.sh
@@ -2,9 +2,12 @@
 # @(#) このプログラムは議事録を整理するためのプログラムです。
 
 SORTEDDIR=sorted_gijiroku
+LOGDIR=gijiroku
 
 if [ -d $SORTEDDIR ]; then
   echo "$SORTEDDIR フォルダはすでに存在します。"
 else
   mkdir $SORTEDDIR
 fi
+
+find $LOGDIR -print
